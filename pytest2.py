6113b90ede10116a336f2787ae839d48734a151e
fatna-sys <fatnamath@gmail.com>
2022-06-30 11:49:19 +0200

    added hungry prompt

--- pytest2.py
+++ pytest2.py
@@ -5,4 +5,8 @@ Created on Thu Jun 30 11:18:22 2022
 @author: fatna
 """
 
-print("this is a message")
+hungry=input("are you hungry?")
+if hungry=="yes":
+    print("eat samosa")
+else:
+    print("do your homework")
